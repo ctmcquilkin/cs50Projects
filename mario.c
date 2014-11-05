12113354ba40463588e9d4c7abc390ba147523f8
Charles McQuilkin <ctmcquilkin@gmail.com>
2014-11-05 12:24:19 -0600

    Started Mario challenge

---
+++ mario.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int n = 0;
+while (n <= 0)
+{
+  int userNumber;
+  char keypressed;
+  char buffer[20];
+  int double num;
+
+  if (userNumber == null)
+  {
+    printf("Please enter the pyramid's height: ");
+    scanf(" %d", &userNumber); getchar();
+ 
+  }  
+//printf("The number you entered was: %d\n", userNumber);
+
+  if (userNumber => 23 || userNumber > 0)
+  {
+    for (i=0; i < userNumber; i++)
+    {
+      printf("#");
+      
+    }
+     
+  } else {
+    printf("Please enter a number between 0 and 23");
+}
+
+  return 0;
+}
